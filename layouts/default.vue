5eac3d6b41dc1eb84e7c36812a183759012902ca
Gaspar Sabater <gaspar@sabater.dev>
2022-11-16 22:58:57 +0100

    guide up to routes

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li>
+          <NuxtLink to="/">Home</NuxtLink>
+        </li>
+
+        <li>---</li>
+        <li>
+          <NuxtLink to="/about">About</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/posts/1">Post 1</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/posts/2">Post 2</NuxtLink>
+        </li>
+        <li>---</li>
+        <li>
+          <NuxtLink to="/misc">MISC</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+    <slot />
+    <AppFooter />
+  </div>
+</template>
---
+++ pages/misc.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Miscellaneous</h1>
+    <p>Here are some miscellaneous things.</p>
+    <ul>
+      <li>
+        Routes - <a href="https://nuxt.com/docs/api/composables/use-route">link</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+
+const route = useRoute()
+// When accessing /posts/1, route.params.id will be 1
+console.log(route, route.params, route.query)
+</script>
